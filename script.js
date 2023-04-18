6523f213d9920b52ff64bb2748807ebb8151c011
hanan3889 <rebaia_hanan@yahoo.fr>
2023-04-18 12:36:30 +0200

    requeste done

--- script.js
+++ script.js
@@ -3,23 +3,19 @@ let ville = "Paris";
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=' 
 + ville + '&appid=558943e66503455f250afa6808ed2879&units=metric';
 
-console.log(url);
-
-// https://api.openweathermap.org/data/2.5/weather?q=saint-saulve&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric
-
 //créer un objet
 let requete = new XMLHttpRequest();
-requete.open('GET', 'url');
+requete.open('GET', url);
 requete.responseType = 'json';
-requete.send(); //Requête envoyer
+requete.send();
 
 requete.onload = function() {
-    if (requete.readyState === XMLHttpRequest.DONE){
+    if (requete.readyState === XMLHttpRequest.DONE) {
         if (requete.status === 200) {
             let reponse = requete.response;
-            console.log(reponse);
+            let temperature = reponse.main.temp;
+            document.querySelector('#temperature_label').textContent = temperature;
+            console.log(temperature);
         }
     }
 }
-// console.log(requete);
-
