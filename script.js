525fdd742ba7517dad859e63540b3d97749dfd51
hanan3889 <rebaia_hanan@yahoo.fr>
2023-04-19 13:46:52 +0200

    ✨translation in html files✨

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 let ville = "Paris";
+recevoirTemperature(ville);
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=' 
-+ ville + '&appid=558943e66503455f250afa6808ed2879&units=metric';
 
-//créer un objet
-let requete = new XMLHttpRequest();
-requete.open('GET', url);
-requete.responseType = 'json';
-requete.send();
 
-requete.onload = function() {
-    if (requete.readyState === XMLHttpRequest.DONE) {
-        if (requete.status === 200) {
-            let reponse = requete.response;
-            let temperature = reponse.main.temp;
-            document.querySelector('#temperature_label').textContent = temperature;
-            console.log(temperature);
+let changerDeVille = document.querySelector('#changer');
+
+changerDeVille.addEventListener('click', () =>{
+    ville = prompt('Which city would you like to see ? ');
+    recevoirTemperature(ville);
+})
+
+
+function recevoirTemperature(ville){
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' 
+    + ville + '&appid=558943e66503455f250afa6808ed2879&units=metric';
+
+    //créer un objet
+    let requete = new XMLHttpRequest();
+    requete.open('GET', url);
+    requete.responseType = 'json';
+    requete.send();
+
+    requete.onload = function() {
+        if (requete.readyState === XMLHttpRequest.DONE) {
+            if (requete.status === 200) {
+                let reponse = requete.response;
+                let temperature = reponse.main.temp;
+                let ville       = reponse.name;
+                console.log(ville);    
+                document.querySelector('#temperature_label').textContent = temperature;
+                document.querySelector('#ville').textContent = ville;
+            }
+        }
+        else {
+            alert('A problem has occurred, please come back later.')
         }
     }
 }
+
+
